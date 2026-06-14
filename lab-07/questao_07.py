177d1013b9a6556bbd1ca230422ecc6f9fa63558
victorslx345 <victor509044@gmail.com>
2026-06-14 11:01:19 -0300

    Update questao_07.py

--- lab-07/questao_07.py
+++ lab-07/questao_07.py
@@ -8,28 +8,32 @@ print('múltiplo de 7 (n % 7 == 0): Avance mais 4 degraus!')
 print('múltiplo de 11 (n % 11 == 0): Volte para o início')
 while True:
     print ('Degrau atual:', degrau)
-    passos = int(input('Quantos passos serão dados? (1-6) '))
+    passos = int(input('Quantos passos serão dados? (1-6) ou 0 para desistir '))
     print ('')
+    if passos == 0:
+        print ('Voce disistiu')
+        break
     if passos < 1 or passos > 6:
         print ('O numero de passos esta fora dos limites')
         print ('')
         continue
     degrau += passos
     print ('')
-    if degrau % 11 == 0:
-        degrau = 1
-        print ('Volte ao inicio')
-    elif degrau % 7 == 0:
-        degrau += 4
-        print ('Avance 4 degraus')
+    if degrau % 3 == 0:
+        degrau -= 1
+        print ('Volte 1 degrau')
     elif degrau % 5 == 0:
         degrau += 1
         print ('Avance 1 degrau')
-    elif degrau % 3 == 0:
-        degrau -= 1
-        print ('Volte 1 degrau')
+    elif degrau % 7 == 0:
+        degrau += 4
+        print ('Avance 4 degraus')
+    elif degrau % 11 == 0:
+        degrau = 1
+        print ('Volte ao inicio')
     print ('')
     if degrau >= 100:
         print ('PARABENS')
         print('Voce ganhou')
+    
     
