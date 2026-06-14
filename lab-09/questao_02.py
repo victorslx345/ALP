48063cde7662166ebe2a917ac6d3fcc74778a4e5
victorslx345 <victor509044@gmail.com>
2026-06-14 10:22:54 -0300

    Add files via upload

---
+++ lab-09/questao_02.py
@@ -0,0 +1,12 @@
+n = int(input('Digite um numero: '))
+if n % 2 == 0:
+    if n > 10:
+        print ('A')
+    elif n == 10:
+        print('B')
+    else:
+        print ('c')
+elif n >5:
+    print ('d')
+else:
+    print('e')
