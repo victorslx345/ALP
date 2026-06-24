ad74cf962efd5b78900cb9483897515420797a52
victorslx345 <victor509044@gmail.com>
2026-06-24 14:59:06 -0300

    Questoes incompletas do lab 12

---
+++ lab-12/questao_02.py
@@ -0,0 +1,20 @@
+
+float(10)
+# Transforma um numero em float.
+int(10.5)
+# Transforma um numero em inteiro apagando os numeros depois da virgula
+a = input('oi')
+# Mostra uma string na tela e le uma string do usuario
+print(a)
+# Mostra uma variavel ou string na tela
+range(1,10,2)
+# Cria uma sequencia de numeros, podendo ser definido o numero de inicio
+# numero final e de quantos em quantos numeros muda
+str(1)
+# Transforma um numero em string
+type(a)
+# Mostra o tipo de uma variavel
+# import
+# Traz uma função da biblioteca
+b = abs(-1)
+# Caso o numero seja negativo ele ficara positivo
---
+++ lab-12/questao_04.py
@@ -0,0 +1,6 @@
+prim = float(input('Digite o primeiro valor: '))
+segun = float(input('Digite o segundo valor: '))
+valor = prim - segun
+valor = abs(valor)
+valor = round(valor,2)
+print('A diferença entre os valores são:', valor)
